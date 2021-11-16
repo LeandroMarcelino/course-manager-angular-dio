da7fe99106c0f3d256a55677b89a57a4a8de2d8b
Leandro Marcelino Souza <leandro.souza.marcelino@gmail.com>
2021-11-15 21:50:06 -0300

    Conhecendo a pasta core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 import { Error404Component } from 'error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     Error404Component,
   ],
   imports: [
--- src/app/courses/course.module.ts
+++ src/app/courses/course.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { CoreModule } from "../core/core.module";
 import { StarModule } from "../shared/component/star/star.module";
 import { CourseInfoComponent } from "./course-info.component";
 import { CourseListComponent } from "./course-list.component";
@@ -17,6 +18,7 @@ import { AppPipeModule } from "./pipe/app-pipe.module";
         CommonModule,
         FormsModule,
         StarModule,
+        CoreModule,
         AppPipeModule,
         RouterModule.forChild([
             {
